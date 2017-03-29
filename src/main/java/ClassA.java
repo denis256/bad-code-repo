81de68f3ed55db9c1bc4d14603ad32f72dcce27d
denis <denis.o@linux.com>
2017-03-29 14:29:47 +0300

    Added more test code

--- src/main/java/ClassA.java
+++ src/main/java/ClassA.java
@@ -1,8 +1,14 @@
 
 public class ClassA {
 
-    private String name;
+    public String name;
     public int age;
+
+
+    public void kappa123(int x, int y) {
+        int k = x * 10;
+        k++;
+    }
 
     public void doCalculations(String[] data) {
         age = 1;
